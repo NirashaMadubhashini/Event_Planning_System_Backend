366bb6ed810eec7f7b396e9d96183fa2bb0fe413
NirashaMadubhashini <nirashamadu0729@gmail.com>
2023-10-23 10:39:26 +0530

    Update OrderRepo.java

--- src/main/java/com/spring/eventPlaning/repo/OrderRepo.java
+++ src/main/java/com/spring/eventPlaning/repo/OrderRepo.java
@@ -1,11 +1,11 @@
 package com.spring.eventPlaning.repo;
 
-import com.spring.eventPlaning.entity.Order;
+import com.spring.eventPlaning.entity.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepo extends JpaRepository<Order, Long> {
+public interface OrderRepo extends JpaRepository<Orders, Long> {
 
 
 }
