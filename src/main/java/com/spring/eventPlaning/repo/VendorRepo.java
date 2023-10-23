74296a452b8bbac5c08a1c7f578d2d25d25c300d
NirashaMadubhashini <nirashamadu0729@gmail.com>
2023-10-23 10:39:38 +0530

    Update VendorRepo.java

--- src/main/java/com/spring/eventPlaning/repo/VendorRepo.java
+++ src/main/java/com/spring/eventPlaning/repo/VendorRepo.java
@@ -3,6 +3,7 @@ package com.spring.eventPlaning.repo;
 import com.spring.eventPlaning.entity.Vendor;
 import com.spring.eventPlaning.enums.AvailabilityStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +11,9 @@ import java.util.List;
 @Repository
 public interface VendorRepo extends JpaRepository<Vendor, Long> {
     List<Vendor> findByAvailability(AvailabilityStatus availabilityStatus);
+
+    @Query("SELECT v FROM Vendor v WHERE v.serviceName = ?1")
+    List<Vendor> findByServiceName(String serviceType);
 
 
 }
